5027f76763dc3b442942b3dcec950d7b7a0252ac
otrack <0track@gmail.com>
2023-05-16 14:01:06 +0200

    add mempool quint specification

---
+++ quint/mempool/test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# consensus
+echo "
+proposeTwiceError
+decideNonProposedError
+decideProposedSuccess
+" | quint -r Consensus.qnt::ConsensusTests
+quint run --main ConsensusTests --invariant invariant Consensus.qnt 
+
+# ledger
+echo "
+submitTwiceError
+commitNonSubmittedError
+commitSubmittedSuccess
+" | quint -r Ledger.qnt::LedgerTests
+quint run --main LedgerTests --invariant invariant Ledger.qnt 
+
+# mempool
+echo "moveHeightOnce" | quint -r Mempool.qnt::MempoolTests
+quint run --verbosity 3 --main MempoolTests --invariant allInv Mempool.qnt 
+
+
