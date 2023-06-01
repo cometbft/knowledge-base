bcf7fd2823d23fa5cc0d952879c32ef8f2b00429
otrack <0track@gmail.com>
2023-06-01 18:42:26 +0200

    - add state machine and test to all the specifications - improve documentation - add stability invariant in Ledger - improve inv4 in Mempool - add gossiping action in Mempool

--- quint/mempool/test.sh
+++ quint/mempool/test.sh
@@ -1,21 +1,6 @@
 #!/bin/sh
 
-# consensus
-echo "
-proposeTwiceError
-decideNonProposedError
-decideProposedSuccess
-" | quint -r Consensus.qnt::ConsensusTests
-quint run --main ConsensusTests --invariant invariant Consensus.qnt 
-
-# ledger
-echo "
-submitTwiceError
-commitNonSubmittedError
-commitSubmittedSuccess
-" | quint -r Ledger.qnt::LedgerTests
-quint run --main LedgerTests --invariant invariant Ledger.qnt 
-
-# mempool
-echo "moveHeightOnce" | quint -r Mempool.qnt::MempoolTests
-quint run --verbosity 3 --main MempoolTests --invariant allInv Mempool.qnt 
+quint test Mempool.qnt
+quint run --init consensusInit --step consensusStep --invariant consensusInvariant Consensus.qnt
+quint run --init ledgerInit --step ledgerStep --invariant ledgerInvariant Ledger.qnt
+quint run --invariant allInv Mempool.qnt 
