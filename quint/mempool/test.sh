d6e8f9596961cf0c63204bc538f1e5387ce3921d
otrack <0track@gmail.com>
2023-06-06 16:34:59 +0200

    - additional refinments

--- quint/mempool/test.sh
+++ quint/mempool/test.sh
@@ -3,4 +3,4 @@
 quint test Mempool.qnt
 quint run --init consensusInit --step consensusStep --invariant consensusInvariant Consensus.qnt
 quint run --init ledgerInit --step ledgerStep --invariant ledgerInvariant Ledger.qnt
-quint run --invariant allInv Mempool.qnt 
+quint run --max-steps 30 --invariant allInv --out-itf out.itf Mempool.qnt 
