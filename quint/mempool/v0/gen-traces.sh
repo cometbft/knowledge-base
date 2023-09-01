87fce5f94386f629ccb701b29a2b3a3ed3493792
hvanz <hernan.vanzetto@gmail.com>
2023-09-01 18:02:06 +0300

    Generate multiple traces with gen script

--- quint/mempool/v0/gen-traces.sh
+++ quint/mempool/v0/gen-traces.sh
@@ -3,7 +3,8 @@
 set -u
 
 PROP=${1:-notFullChain}
-[[ ! -z "${PROP}" ]] && echo "Generating trace for ${PROP}..." || (echo "PROP is empty" && exit 1)
+NUM=${2:-1}
+[[ ! -z "${PROP}" ]] && echo "Generating $NUM trace(s) for ${PROP}..." || (echo "PROP is empty" && exit 1)
 
 TRACES_DIR="traces/$PROP"
 mkdir -p "$TRACES_DIR"
@@ -20,14 +21,14 @@ function nextFilename() {
     echo $name.$ext
 }
 
-FILE_NAME=$(nextFilename $TRACES_DIR "${PROP}_trace" "itf.json")
-TRACE_PATH="$TRACES_DIR/$FILE_NAME"
-echo "Generating trace: $TRACE_PATH"
-
-time quint run \
-    --verbosity 5 \
-    --max-steps=100 \
-    --max-samples=3 \
-    --invariant "$PROP" \
-    --out-itf "$TRACE_PATH" \
-    mempoolv0.qnt
+for i in `seq 1 $NUM`; do
+    FILE_NAME=$(nextFilename $TRACES_DIR "${PROP}_trace" "itf.json")
+    TRACE_PATH="$TRACES_DIR/$FILE_NAME"
+    echo "Generating trace for $PROP: $TRACE_PATH"
+    time quint run \
+        --verbosity 5 \
+        --max-steps=300 \
+        --invariant "$PROP" \
+        --out-itf "$TRACE_PATH" \
+        mempoolv0.qnt
+done
